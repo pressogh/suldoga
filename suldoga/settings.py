198f18697e42275fb7498629118e91b7f52e90d4
PRESSO <caff1nepill@gmail.com>
2022-08-11 19:18:03 +0900

    change database

--- suldoga/settings.py
+++ suldoga/settings.py
@@ -97,13 +97,7 @@ WSGI_APPLICATION = 'suldoga.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = get_secret("DATABASES")['default']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
