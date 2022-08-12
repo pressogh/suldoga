37e99334f04870859a383ca44ac1f373236ff67d
LINKER <temp2temp@daum.net>
2022-08-13 03:12:30 +0900

    static error로 인한 임시조치 (cocktail / k-ocktail style fix)

--- cocktail/static/cocktail/list/js/common.js
+++ cocktail/static/cocktail/list/js/common.js
@@ -1,19 +1,21 @@
-window.onload = (function(){
-    $("#keyword").keyup(function() {
+window.onload = function () {
+  $('#keyword').keyup(function () {
     var k = $(this).val();
-    $(".coclist > .cardBox").hide();
-    var temp = $(".coclist > .cardBox > .titleKr+.titleEng:contains('" + k + "')");
+    $(' .coclist > .cocktailDiv >.cardBox').hide();
+    var temp = $(
+      ".coclist > .cocktailDiv > .cardBox > .titleKr +.titleEng:contains('" +
+        k +
+        "')"
+    );
 
     $(temp).parent().show();
-})
+  });
 
-let check = true; // 바뀌기 때문에 let으로 선언
-function changeLike() {
-  document.getElementById('img').src = check
-    ? 'img/unlike.png' //연산자 이용 체크
-    : 'img/like.png';
+  let check = true; // 바뀌기 때문에 let으로 선언
+  function changeLike() {
+    document.getElementById('img').src = check
+      ? 'img/unlike.png' //연산자 이용 체크
+      : 'img/like.png';
     check = !check;
-}
-
-})
-
+  }
+};
