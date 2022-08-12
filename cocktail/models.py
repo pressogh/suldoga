16ed4263ad4e6a825437be41379b90551da688af
PRESSO <caff1nepill@gmail.com>
2022-08-13 05:20:33 +0900

    revert Cocktail model

--- cocktail/models.py
+++ cocktail/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Cocktail(models.Model):
+    name = models.CharField(max_length=20)
+    eng_name = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='cocktail/')
+    alcohol = models.CharField(max_length=20)
+    taste = models.CharField(max_length=20)
+    base = models.CharField(max_length=20)
+    match_food = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
