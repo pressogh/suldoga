952b35ef3acf090bcec9b0897f457a9e3130bec3
leeeeyz77 <102798337+leeeeyz77@users.noreply.github.com>
2022-08-13 13:05:15 +0900

    2

--- board/forms.py
+++ board/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from django.forms import widgets
-from .models import Board
+from .models import Post
 
 class RegistForm(forms.ModelForm):
     class Meta:
-        model = Board
-        fields = ['title', 'author', 'body']
+        model = Post
+        fields = ['title', 'user', 'content']
         widgets = {
             'title': forms.TextInput(attrs={'class':'form-control'}),
-            'author': forms.TextInput(attrs={'class':'form-control'}),
-            'body' : forms.Textarea(attrs={'class': 'form-control', 'rows':10}),
+            'user': forms.TextInput(attrs={'class':'form-control'}),
+            'content' : forms.Textarea(attrs={'class': 'form-control', 'rows':10}),
         }
--- board/views.py
+++ board/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from .models import Post
+from .forms import RegistForm
 
-# Create your views here.
+def BoardView(request):
+    board_list = Post.objects.all().order_by('-created_at')
+    context = {'board_list': board_list}
+    return render(request, 'board/board.html', context)
+
+
+def regist(request):
+    if request.method == 'POST':
+        form = RegistForm(request.POST)
+        if form.is_valid():
+             form.save()
+             return redirect('board:board')
+    else:
+        form = RegistForm()
+    context = {'form': form,}
+    return render(request, 'board/regist.html', context)
+
+def detail(request, pk):
+    board_list = get_object_or_404(Post, id=pk)
+    context = {'board_list': board_list}
+    return render(request, 'board/detail.html', context)
