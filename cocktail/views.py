489169c810178951f1071cd91affad1984694f17
jihyunna <skwlgus106@naver.com>
2022-08-19 02:24:10 +0900

    url

--- cocktail/views.py
+++ cocktail/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect, get_object_or_404
-from .models import Cocktail, Like, Star
+from .models import Cocktail
 from django.core.paginator import Paginator
+from accounts.models import User
 
 
 def MainView(request):
@@ -13,12 +14,55 @@ def InfoView(request):
 
 def ListView(request):
     cocktail = Cocktail.objects.filter(type="C")    # 테이블의 객체 불러와서 저장
-    return render(request, 'cocktail/cocktail.html', {"cocktail": cocktail})
+    sort = request.GET.get('sort','')    
+
+    if sort == 1: #기본
+        cocktail_list= Cocktail.objects.order_by('')
+        #return render(request, 'cocktail/cocktail.html', {"cocktail": cocktail_list})
+
+    elif sort == 2: #스크랩순 (하트)
+        cocktail_list = Cocktail.objects.order_by(like_count=Cocktail('likes')).order_by('-like_count')
+        #return render(request, 'cocktail/cocktail.html', {"cocktail": cocktail_list})
+
+    elif sort == 3: #도수 낮은 순
+        cocktail_list = Cocktail.objects.order_by('alcohol')
+        #return render(request, 'cocktail/cocktail.html', {"cocktail": cocktail_list})
+
+    elif sort == 4: #도수 높은 순
+        cocktail_list = Cocktail.objects.order_by('-alcohol')
+        #return render(request, 'cocktail/cocktail.html', {"cocktail": cocktail_list})
+
+    paginator=Paginator(cocktail_list,16)
+    page = request.GET.get('page','')
+    cocktails = paginator.get_page(page)
+
+    #return render(request, 'cocktail/cocktail.html', {"cocktail": cocktail})
+
+    return render(request, 'cocktail/cocktail.html', {'cocktails':cocktails,'sort':sort })
 
 
 def KListView(request):
     kocktail = Cocktail.objects.filter(type="K")
+    sort = request.GET.get('sort','')    
+
+    if sort == 1: #기본
+        kocktail_list= Cocktail.objects.order_by('')
+        return render(request, 'cocktail/kocktail.html', {"cocktail": kocktail_list})
+
+    elif sort == 2: #스크랩순 (하트)
+        kocktail_list = Cocktail.objects.annotate(like_count=Cocktail('likes')).order_by('-like_count')
+        return render(request, 'cocktail/kocktail.html', {"cocktail": kocktail_list})
+
+    elif sort == 3: #도수 낮은 순
+        kocktail_list = Cocktail.objects.order_by('alcohol')
+        return render(request, 'cocktail/kocktail.html', {"cocktail": kocktail_list})
+
+    elif sort == 4: #도수 높은 순
+        kocktail_list = Cocktail.objects.order_by('-alcohol')
+        return render(request, 'cocktail/kocktail.html', {"cocktail": kocktail_list})
+    
     return render(request, 'cocktail/kocktail.html', {"kocktail": kocktail})
+
 
 def Combination2View(request):
     return render(request, 'cocktail/combination2.html') 
@@ -28,8 +72,7 @@ def TestView(request):
     return render(request, 'cocktail/test.html')
 
 
-
-def LikeView(request, cocktail_id):
+"""def LikeView(request, cocktail_id):
     like_c = get_object_or_404(Cocktail, id=cocktail_id)
 
     if request.user in like_c.likes.all():
@@ -42,38 +85,30 @@ def LikeView(request, cocktail_id):
         like_c.like_count += 1
         like_c.save()
         
-    return redirect('cocktail/:test2', like_c.id)
+    return redirect('cocktail/test2', like_c.id)
 
+"""
+def LikeView(request, cocktails_id):
+    if request.user.is_authenticated:
+        cocktails = get_object_or_404(Cocktail, id=cocktails_id)
 
+        if cocktails.like.filter(id=request.user.id).exists():
+            cocktails.like.remove(request.user)
+            cocktails.like_count -= 1
+            cocktails.save()
+        else:
+            cocktails.like.add(request.user) 
+            cocktails.like_count += 1
+            cocktails.save()   
+        return redirect('cocktail:main')
+    return redirect('accounts:login') 
 
-def index(request):
-    sort = request.GET.get('sort','')
-    paginator = Paginator(cocktail_list,'10')
-    page = request.GET.get('page','')
-    posts = paginator.get_page(page)
-
-    if sort == 1: #기본
-        cocktail_list= Cocktail.objects.order_by('')
-
-    elif sort == 2: #별점순
-        cocktail_list = Cocktail.objects.annotate(star_count=Cocktail('stars')).order_by('-star_count')
-        
-    elif sort == 3: #스크랩순
-        cocktail_list = Cocktail.objects.annotate(like_count=Cocktail('likes')).order_by('-like_count')
-
-    elif sort == 4: #도수순
-        cocktail_list = Cocktail.objects.order_by('alcohol')
-
-
-    elif sort == 5: #최신순
-        cocktail_list = Cocktail.objects.filter(name_id = user).order_by('-date') 
- 
-
-    elif sort == 6: #여성인기
-        cocktail_list = Cocktail.objects.order_by('')
-
-    else: #남성인기
-        cocktail_list = Cocktail.objects.order_by('')
-        return render(request, 'cocktail/cocktail.html', {"cocktail": cocktail_list})
+def myprofile(request,user_id):  
+    user = User.objects.get(id = user_id)
+    cocktails = user.like.all()
+    context={
+        "cocktails":cocktails
+    }
+    return render(request, 'cocktail/myprofile.html',context)
 
     
