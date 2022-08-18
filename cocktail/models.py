58fe789c87f436eda1b980a5dd09cdce129dd2cd
jihyunna <skwlgus106@naver.com>
2022-08-19 02:21:11 +0900

    change alcohol field type

--- cocktail/models.py
+++ cocktail/models.py
@@ -7,35 +7,15 @@ class Cocktail(models.Model):
     eng_name = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
     image = models.ImageField(upload_to='cocktail/')
-    alcohol = models.CharField(max_length=20)
+    alcohol = models.IntegerField()
     taste = models.CharField(max_length=20)
     base = models.CharField(max_length=20)
     match_food = models.CharField(max_length=20)
     type = models.CharField(max_length=1, choices=(('C', 'Cocktail'), ('K', 'K-ocktail')))
 
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, through='Like', related_name='like_cocktail', blank=True)
-    stars = models.ManyToManyField(User, through='Star', related_name='star_cocktail', blank=True)
-
+    like = models.ManyToManyField(User, related_name='like', blank=True)
     like_count = models.PositiveIntegerField(default=0)
-    star_count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
-
-
-class Like(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    cocktail = models.ForeignKey(Cocktail,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.cocktail
-
-
-class Star(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    cocktail = models.ForeignKey(Cocktail,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.cocktail
 
