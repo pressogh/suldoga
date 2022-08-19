b1b0b85543c78f2ce86c2af1941451226a3e3ce2
PRESSO <caff1nepill@gmail.com>
2022-08-19 23:51:39 +0900

    change settings

--- suldoga/settings.py
+++ suldoga/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_URL = 'workspace/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'workspace/static/'),)
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
