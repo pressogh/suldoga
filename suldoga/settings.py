c9a960d6dcfe7e99b9e6b0e9515f5b11a86a1a44
PRESSO <caff1nepill@gmail.com>
2022-08-20 00:25:15 +0900

    change settings

--- suldoga/settings.py
+++ suldoga/settings.py
@@ -130,11 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/workspace/static',
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+print(STATIC_ROOT)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
