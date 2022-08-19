186832f2322bd0236907bb228a3e90007d78b8cd
PRESSO <caff1nepill@gmail.com>
2022-08-20 00:22:35 +0900

    change settings

--- suldoga/settings.py
+++ suldoga/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/workspace/static',
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
